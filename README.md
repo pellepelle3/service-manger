f379a086ea70638f367a6e86bb7f77d74bbe813e
Antonio Pellegrino <pellepelle3@gmail.com>
2015-05-22 19:37:39 -0400

    init

---
+++ README.md
@@ -0,0 +1,152 @@
+Service Manager
+===
+
+- Apis
+- Config
+
+APIs
+---
+### POST: /api/v1/service/add/:service
+
+Adds to service discovery 
+and to secret store
+
+Payload:
+```json
+{
+  "host" : "127.0.0.1",
+  "port": 1234,
+  "conf":{ //optional
+    // your json conf
+  }
+}
+```
+
+Response Success:
+Http Code 200
+
+Response Error:
+Http Code 422
+
+
+### POST: /api/v1/service/remove/:service
+
+Removes to service discovery 
+and from secret store
+
+Payload:
+```json
+{
+  "host" : "127.0.0.1",
+  "port": 1234,
+}
+```
+
+Response Success:
+Http Code 200
+
+Response Error:
+Http Code 422
+
+
+### POST: /api/v1/service/config/:service
+
+Update/Add to secret store
+
+Payload:
+```json
+{
+ // json config data
+}
+```
+
+Response Success:
+Http Code 200
+
+Response Error:
+Http Code 422
+
+### GET: /api/v1/service/config/:service
+
+Read from secret store
+
+Response Success:
+Http Code 200
+```json
+{
+ // json config data
+}
+```
+
+
+Response Error:
+Http Code 422
+
+
+### GET: /api/v1/service/list
+
+Read from secret store
+
+Response Success:
+Http Code 200
+```json
+[
+  "health",
+  "www"
+ // array of service names
+]
+```
+
+### GET: /api/v1/service/list/detail
+
+Read from secret store with more details
+
+Response Success:
+Http Code 200
+```json
+[
+  [
+    {
+      Node: "local",
+      Address: "127.0.0.1",
+      ServiceID: "email",
+      ServiceName: "email",
+      ServiceTags: null,
+      ServiceAddress: "",
+      ServicePort: 55395
+    }
+  ],
+  [
+    {
+      Node: "local",
+      Address: "127.0.0.1",
+      ServiceID: "health",
+      ServiceName: "health",
+      ServiceTags: null,
+      ServiceAddress: "",
+      ServicePort: 55396
+    }
+  ]
+]
+```
+
+Config
+---
+
+Go to the config tab and set it with 
+You can add what ever auth to login with user as key and pass as value.
+It will be open when deployed make sure to password protech after
+
+```json
+{
+  "auth":{
+    "user":"passward"
+  }
+}
+
+```
+
+
+
+
+
---
+++ api/v1/index.js
@@ -0,0 +1,220 @@
+var express = require('express')
+  , api = express.Router()
+  , request = require('request')
+  , Url = require('url')
+  , lsq = require('lsq')
+  , consul 	= require('lsq-consul')({ host: process.env.CONSUL_HOST, port: process.env.CONSUL_PORT })
+  , _ = require('underscore')
+  , Promise = require('promise')
+  , auth = require("basic-auth")
+  , sq
+  , config
+
+updateList()
+
+function updateList(){
+	var watcher = consul.watch(consul.kv.get, { key: process.env.SERVICE_NAME })
+	watcher.on('change', function(result) {
+		 try {
+			if (!result) throw new Error('configuration not present')
+			config = JSON.parse(result.Value)
+		}
+		catch (e) {}
+	})
+	watcher.on('error', function(err) {
+	  console.log('error:', err)
+	})
+}
+
+api.get('/',function(req,res){
+  res.send(req.headers.host)
+})
+
+api.post('/service/add/:service',function(req,res){
+	basicAuth(req,res).then(function(){
+		var json = req.body
+		var service = req.params.service
+		var status = 200
+
+		if(!json.host || !json.port){
+			json = { error:"missing host or port" }
+			status = 422
+		}
+
+		if(!json.error && !service){
+			json = { error:"missing service" }
+			status = 422
+		}
+
+		if(status!=200){
+			res.status(status)
+	      return res.send(json)
+		}
+
+		if(_.isObject(json.conf))
+			consul.kv.set(service,JSON.stringify(json.conf),function(err,result){
+			})
+
+		consul.agent.service.register({
+	        name: service,
+	        port: json.port,
+	        check: {
+	          http: Url.format({ protocol: 'http',
+	                             hostname: json.host,
+	                             port: json.port,
+	                             pathname:'/health'
+	                           }),
+	          interval: '1s'
+	    	}
+		},function(err,result){
+			if(err || !result) {
+				status = 422 
+				err = !err ? "missing service" : err
+				result = {value:{error:err}}
+			}
+			res.send(result.Value)
+		})
+	})
+})
+
+api.post('/service/remove/:service',function(req,res){
+	basicAuth(req,res).then(function(){
+		var json = req.body
+		var service = req.params.service
+		var status = 200
+	   	
+		if(!json.error && !service){
+			json = {error:"missing service"}
+			status = 422
+		}
+
+		if(status!=200){
+			res.status(status)
+	      return res.send(json)
+		}
+
+		consul.kv.del(service,function(err,result){
+			console.log("error on del key",service,err)
+		})
+		consul.agent.service.deregister(service,function(err,result){
+			if(err || !result) {
+				status = 422 
+				err = !err ? "missing service" : err
+				result = {value:{error:err}}
+			}
+			res.send(result.Value)
+		})
+	})
+	
+})
+
+api.post('/service/config/:service',function(req,res){
+	basicAuth(req,res).then(function(){
+
+		var json = req.body
+		var service = req.params.service
+		var status = 200
+
+		if(!json.error && !service){
+			json = {error:"missing service"}
+			status = 422
+		}
+
+		if(status!=200){
+			res.status(status)
+	      return res.send(json)
+		}
+
+		consul.kv.set(service,JSON.stringify(json),function(err,result){
+			if(err || !result) {
+				status = 422 
+				err = !err ? "missing service" : err
+				result = {value:{error:err}}
+			}
+			res.send(result.Value)
+		})
+	})
+
+})
+
+api.get('/service/config/:service',function(req,res){
+	basicAuth(req,res).then(function(){
+		var json = {}
+		var service = req.params.service
+		var status = 200
+
+		if(!json.error && !service){
+			json = {error:"missing service"}
+			status = 422
+		}
+
+		if(status!=200){
+			res.status(status)
+	      return res.send(json)
+		}
+
+		consul.kv.get(service,function(err,config){
+
+			if(err || !config) {
+				status = 422 
+				err = !err ? "missing config" : err
+				config = {value:{error:err}}
+			}
+			
+			res.status(status)
+	      	res.send(config.Value)
+		})
+	})
+})
+
+api.get('/service/list',function(req,res){
+	basicAuth(req,res).then(function(){
+		lsq.services.list()
+	  	.then(function(services){
+	  		res.send(services || {})
+	  	})
+	})
+})
+
+api.get('/service/list/detail',function(req,res){
+	basicAuth(req,res).then(function(){
+		var promises = []
+		lsq.services.list()
+	  	.then(function(services){
+			return services.map(function(service) { return detail(service)})
+	    })
+	    .then(Promise.all)
+	    .then(function(results){
+	    	res.send(results)
+	    })
+	})
+})
+
+function basicAuth(req,res){
+	return new Promise(function(resolve,reject){
+		if(!_.isObject(config.auth)) return resolve()
+		
+		var credentials = auth(req)
+		if (credentials  
+		&& _.has(config.auth,credentials.name)
+		&& config.auth[credentials.name] == credentials.pass) 
+			return resolve()
+
+		res.writeHead(401, {
+			'WWW-Authenticate': 'Basic realm="Auth"'
+		})
+		res.end()
+		reject() 
+	})
+}
+
+function detail(service){
+	return new Promise(function(resolve, reject) {
+		consul.catalog.service.nodes(service,function(err, services) {
+		  if (err) return reject(err)
+		  resolve(services)
+		})
+	})
+}
+module.exports = api
+
